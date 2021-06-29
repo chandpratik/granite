52581d4f7b2a37dce7caab9998bce261f221623c
pratikal1 <chandpratikal1@gmail.com>
2021-06-30 01:46:29 +0530

    Created Toastr component and added Axios interceptors

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,20 +1,24 @@
 import React, { useEffect, useState } from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
 import CreateTask from "components/Tasks/CreateTask";
 import Dashboard from "components/Dashboard";
 
-import { setAuthHeaders } from "./apis/axios";
+import { registerIntercepts, setAuthHeaders } from "./apis/axios";
 import { initializeLogger } from "./common/logger";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
   useEffect(() => {
     initializeLogger();
+    registerIntercepts();
     setAuthHeaders(setLoading);
   }, []);
 
   return (
     <Router>
+      <ToastContainer />
       <Switch>
         <Route exact path="/tasks/create" component={CreateTask} />
         <Route exact path="/dashboard" component={Dashboard} />
